9b44031944540cb317c56fe2348b8be85b0d90ce
virangdoshi <virangdoshi@users.noreply.github.com>
2020-07-13 21:13:24 -0400

    Update truffle-config.js
    Change the default port for ganache

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*",
       contractAddress: "0xc156327be8e1B2b61e72433142407fB3C6ed067D"
     }
